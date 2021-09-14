2cb6d19c763fe56017eb10b4e4eabc720aa56e7f
Bruno Carneiro <bruno_graciano@outlook.com>
2021-09-13 21:20:05 -0300

    Exercicio 1

---
+++ src/main/java/curso_programacao/Exercicio_1.java
@@ -0,0 +1,26 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double valorTerreno = sc.nextDouble();
+
+		double area = largura * comprimento;
+		double precoTerreno = area * valorTerreno;
+
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.printf("PRECO = %.2f%n", precoTerreno);
+
+		sc.close();
+	}
+
+}
